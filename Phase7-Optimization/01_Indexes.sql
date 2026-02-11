5303bccd67e651cb89f10c7158f151bc3fba26d8
Wajid Rashid <i.wajidrashid18@gmail.com>
2026-02-11 18:47:06 +0500

    Indexes

--- Phase7-Optimization/01_Indexes.sql
+++ Phase7-Optimization/01_Indexes.sql
@@ -9,15 +9,3 @@ create index idx_menuitems_name on menuitems(item_name);
 create index idx_restaurants_city_cuisine on restaurants(city_id, cuisine_type);
 
 create index idx_history_order_time on orderstatushistory(order_id, changed_at);
-
-explain select * from orders where customer_id = 5;
-
-explain select r.name, count(o.order_id) from restaurants r join orders o on r.restaurant_id = o.restaurant_id group by r.restaurant_id;
-
-explain select * from menuitems where item_name like 'chicken%';
-
-explain select * from orders where order_status = 'pending';
-
-create index idx_test_pending on orders(order_status);
-
-explain select * from orders where order_status = 'pending';
